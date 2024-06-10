7df3612937bd4cb6582f91892ef4c9addd8de591
off_jpedro <jpedroribeirodasilva@hotmail.com>
2024-06-10 17:41:38 -0300

    subindo ajustes na service

--- Application/Services/CarBrandService.cs
+++ Application/Services/CarBrandService.cs
@@ -1,12 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Interfaces;
+using Domain.Models;
+using Domain.Validators.Interfaces;
+using VarejoWeb.Service.Interfaces;
 
 namespace VarejoWeb.Service
 {
-    public class CarBrandService
+    public class CarBrandService : ICarBrandService
     {
+        private readonly new ICarBrandRepository _carBrandRepository;
+        private readonly new ICarBrandValidator _carBrandValidator;
+
+        public CarBrandService(ICarBrandRepository carBrandrepository, ICarBrandValidator carBrandValidator)
+        {
+            _carBrandRepository = carBrandrepository;
+            _carBrandValidator = carBrandValidator;
+        }
+       
+        public async Task<IEnumerable<CarBrand>> GetAll()
+        {
+            var  result = await _carBrandRepository.GetAll();
+            return result;
+        }
+
+        public Task<CarBrand> GetById(long id)
+        {
+        }
+
+        public Task<bool> Insert(CarBrand entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(CarBrand entity)
+        {
+            throw new NotImplementedException();
+        }
         
+         public Task<bool> Delete(CarBrand entity)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
